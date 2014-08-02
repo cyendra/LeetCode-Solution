736648ec34e40346a5adcae5b7085aa48a807fe6
cyendra <g1483335912@gmail.com>
2014-08-02 17:55:07 +0800

    2

---
+++ LeetCode_Solution/Median of Two Sorted Arrays.h
@@ -0,0 +1,6 @@
+class Solution {
+public:
+	double findMedianSortedArrays(int A[], int m, int B[], int n) {
+
+	}
+};
---
+++ LeetCode_Solution/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "Two Sum.h"
+
+using namespace std;
+
+Solution ss;
+
+int main()
+{
+	vector<int> v;
+	cout << ss.twoSum(v, 6)[0] << endl;
+	return 0;
+}
