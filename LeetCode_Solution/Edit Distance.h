c3cb00d865c8663fd0c74ea4208157c9427276cc
cyendra <g1483335912@gmail.com>
2014-08-08 22:34:11 +0800

    ED

--- LeetCode_Solution/Edit Distance.h
+++ LeetCode_Solution/Edit Distance.h
@@ -19,14 +19,15 @@ public:
 			}
 		}
 		f[0][0] = 0;
-		f[0][1] = 0;
-		f[1][0] = 0;
+		for (int i = 1; i <= n; i++) f[i][0] = i;
+		for (int i = 1; i <= m; i++) f[0][i] = i;
 		for (int i = 1; i <= n; i++)
 		{
 			for (int j = 1; j <= m; j++)
 			{
 				if (word1[i - 1] == word2[j - 1]) f[i][j] = f[i - 1][j - 1];
-				else f[i][j] = min(f[i - 1][j], f[i][j - 1]) + 1;
+				else f[i][j] = f[i - 1][j - 1] + 1;
+				f[i][j] = min(f[i][j], min(f[i - 1][j], f[i][j - 1]) + 1);
 			}
 		}
 		return f[n][m];
