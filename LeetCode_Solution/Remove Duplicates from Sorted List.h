1dc5a766a92bd5346ab5eaa222f4e3a4d9266958
cyendra <g1483335912@gmail.com>
2014-08-10 22:14:07 +0800

    rmv

---
+++ LeetCode_Solution/Remove Duplicates from Sorted List.h
@@ -0,0 +1,14 @@
+#include "std.h"
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode *deleteDuplicates(ListNode *head) {
+
+	}
+};
