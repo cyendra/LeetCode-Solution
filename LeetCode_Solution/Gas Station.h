2b4e89c54f2b991949de678c52ca73a0188eab1f
cyendra <g1483335912@gmail.com>
2014-08-27 21:04:20 +0800

    gas

---
+++ LeetCode_Solution/Gas Station.h
@@ -0,0 +1,13 @@
+#include "std.h"
+
+class Solution {
+public:
+	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+		priority_queue<int> que;
+		int n = gas.size();
+		for (int i = 0; i < n; i++)
+		{
+
+		}
+	}
+};
