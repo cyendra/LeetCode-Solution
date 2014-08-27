0b866c2989b9edd447ee50cc5592eb0a77261dd1
cyendra <g1483335912@gmail.com>
2014-08-27 21:27:21 +0800

    GASS

--- LeetCode_Solution/Gas Station.h
+++ LeetCode_Solution/Gas Station.h
@@ -3,11 +3,46 @@
 class Solution {
 public:
 	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
-		priority_queue<int> que;
+		vector<int> a;
 		int n = gas.size();
 		for (int i = 0; i < n; i++)
 		{
-
+			a.push_back(gas[i] - cost[i]);
+		}
+		for (int i = 0; i < n; i++)
+		{
+			a.push_back(a[i]);
+		}
+		int sum = 0;
+		int cnt = 0;
+		int ans = 0;
+		int p = 0;
+		for (int i = 0; i < 2 * n; i++)
+		{
+			if (sum + a[i] >= 0)
+			{
+				cnt++;
+				if (cnt > ans)
+				{
+					ans = cnt;
+					p = i;
+				}
+				sum += a[i];
+			}
+			else
+			{
+				sum = 0;
+				cnt = 0;
+			}
+			if (ans == n) break;
+		}
+		if (ans >= n)
+		{
+			return p + 1 - n;
+		}
+		else
+		{
+			return -1;
 		}
 	}
 };
