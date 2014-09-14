a0c8cdaa4539b2db14176fa568f7cc16c4502403
cyendra <g1483335912@gmail.com>
2014-09-14 21:22:55 +0800

    gg

--- LeetCode_Solution/Add Two Numbers.h
+++ LeetCode_Solution/Add Two Numbers.h
@@ -9,6 +9,37 @@ struct ListNode {
 class Solution {
 public:
 	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-
+		if (l1 == NULL) return l2;
+		if (l2 == NULL) return l1;
+		ListNode *p = new ListNode(0);
+		ListNode *h = p;
+		while (l1 != NULL && l2 != NULL) {
+			p->val = l1->val + l2->val;
+			l1 = l1->next;
+			l2 = l2->next;
+			if (l1 != NULL || l2 != NULL) p->next = new ListNode(0);
+			if (p->val >= 10) {
+				p->val -= 10;
+				if (p->next != NULL) p->next->val += 1;
+				else p->next = new ListNode(1);
+			}
+			p = p->next;
+		}
+		if (l1 == NULL && l2 == NULL) return h;
+		ListNode *lst;
+		if (l1 != NULL) lst = l1;
+		else lst = l2;
+		while (lst != NULL) {
+			p->val += lst->val;
+			lst = lst->next;
+			if (lst != NULL) p->next = new ListNode(0);
+			if (p->val >= 10) {
+				p->val -= 10;
+				if (p->next != NULL) p->next->val += 1;
+				else p->next = new ListNode(1);
+			}
+			p = p->next;
+		}
+		return h;
 	}
 };
